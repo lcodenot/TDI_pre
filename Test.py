bcb328d349888b34e460224204817b6eb1a8bd88
Luca <lcodenot@gmail.com>
2017-06-18 20:35:31 +0000

    add graph

--- Test.py
+++ Test.py
@@ -2,27 +2,15 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 import requests
 
+import sys
+
 from bokeh.io import push_notebook, show, output_notebook
 from bokeh.layouts import row
 from bokeh.plotting import figure
 from bokeh.embed import components 
 output_notebook()
 
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-  return redirect('/index')
-
-@app.route('/index')
-def index():
-  return render_template('index.html')
-
-if __name__ == '__main__':
-     app.run(host='0.0.0.0') 
-    #app.run(port=33507)
-
-response = 'FB'
+response = raw_input("Insert Tick i.e. GOOG: ")
 
 def picture(tick):
     api_url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20170501&date.lt=20170601&ticker=%s&api_key=TNvqvN5w-dbDua3bVNzk' %tick
@@ -36,11 +24,8 @@ def picture(tick):
 
     p = figure(plot_width = 500, plot_height = 500, title='Data from Quandle WIKI set', x_axis_label='date', x_axis_type='datetime')
     r = p.line(df_important['date'],df_important['close'], line_width = 1)
-    t = show(p, notebook_handle = True)
-    #script, div = components(p)
-    #return render_template('graph.html', script=script, div=div)
+    #t = show(p, notebook_handle = True)
+    script, div = components(p)
+    return render_template('graph.html', script=script, div=div)
 
 picture(response)
-
-
-
