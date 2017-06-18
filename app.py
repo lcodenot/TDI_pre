5292581295cec1ab0d00a6b2e08b30acc7fedd88
Luca <lcodenot@gmail.com>
2017-06-18 19:53:33 +0000

    html embedded graph

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import requests
 from bokeh.io import push_notebook, show, output_notebook
 from bokeh.layouts import row
 from bokeh.plotting import figure
+from bokeh.embed import components 
 output_notebook()
 
 app = Flask(__name__)
@@ -30,9 +31,13 @@ df = pd.DataFrame(data['datatable']['data'])
 df_important= df[[1,5,12,2,9]]
 df_important.columns = ['date','close','adjusted close','open','adjusted open']
 
-p = figure(plot_width = 500, plot_height = 500)  
-r = p.multi_line([[1,2,3,4,5,8,9,10,11,12,15,16,17,18,19,22,23,24,25,26,30,31],[1,2,3,4,5,8,9,10,11,12,15,16,17,18,19,22,23,24,25,26,30,31],[1,2,3,4,5,8,9,10,11,12,15,16,17,18,19,22,23,24,25,26,30,31],[1,2,3,4,5,8,9,10,11,12,15,16,17,18,19,22,23,24,25,26,30,31]],[df_important['close'],df_important['open'],df_important['adjusted close'],df_important['adjusted open']], line_width = 1)
-t = show(p, notebook_handle = True)
+p = figure(plot_width = 500, plot_height = 500, title='Data from Quandle WIKI set', x_axis_label='date', x_axis_type='datetime')
+r = p.line(df_important['date'],df_important['close'], line_width = 1)
+script, div = components(plot)
+return render_template('graph.html', script=script, div=div)
+
+
+
 <<<<<<< HEAD
 
 =======
