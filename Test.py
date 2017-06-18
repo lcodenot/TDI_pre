e647e5b4f8c0f9728692f2679cbbe76e8303d942
Luca <lcodenot@gmail.com>
2017-06-18 20:51:37 +0000

    add graph

--- Test.py
+++ Test.py
@@ -11,8 +11,10 @@ from bokeh.embed import components
 output_notebook()
 
 response = raw_input("Insert Tick i.e. GOOG: ")
+yesno = [True,True,True,True]
 
-def picture(tick):
+
+def picture(tick, graphs):
     api_url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20170501&date.lt=20170601&ticker=%s&api_key=TNvqvN5w-dbDua3bVNzk' %tick
 
     r = requests.get(api_url)
@@ -22,10 +24,11 @@ def picture(tick):
     df_important= df[[1,5,12,2,9]]
     df_important.columns = ['date','close','adjusted close','open','adjusted open']
 
+    df_important['date'] = df_important['date'].astype('datetime64[ns]')
     p = figure(plot_width = 500, plot_height = 500, title='Data from Quandle WIKI set', x_axis_label='date', x_axis_type='datetime')
     r = p.line(df_important['date'],df_important['close'], line_width = 1)
-    #t = show(p, notebook_handle = True)
-    script, div = components(p)
-    return render_template('graph.html', script=script, div=div)
+    t = show(p, notebook_handle = True)
+    #script, div = components(p)
+    #return render_template('graph.html', script=script, div=div)
 
-picture(response)
+picture(response, yesno)
--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 import requests
 
+import sys
+
 from bokeh.io import push_notebook, show, output_notebook
 from bokeh.layouts import row
 from bokeh.plotting import figure
@@ -16,15 +18,7 @@ def main():
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
-
-if __name__ == '__main__':
-     app.run(host='0.0.0.0') 
-    #app.run(port=33507)
-
-response = 'FB'
-
-def picture(tick):
+    tick = 'FB'
     api_url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20170501&date.lt=20170601&ticker=%s&api_key=TNvqvN5w-dbDua3bVNzk' %tick
 
     r = requests.get(api_url)
@@ -33,14 +27,20 @@ def picture(tick):
 
     df_important= df[[1,5,12,2,9]]
     df_important.columns = ['date','close','adjusted close','open','adjusted open']
+    df_important['date'] = df_important['date'].astype('datetime64[ns]')
 
     p = figure(plot_width = 500, plot_height = 500, title='Data from Quandle WIKI set', x_axis_label='date', x_axis_type='datetime')
     r = p.line(df_important['date'],df_important['close'], line_width = 1)
-    t = show(p, notebook_handle = True)
-    #script, div = components(p)
-    #return render_template('graph.html', script=script, div=div)
+    #t = show(p, notebook_handle = True)
+    script, div = components(p)
+    return render_template('graph.html', script=script, div=div)
 
-picture(response)
+if __name__ == '__main__':
+     app.run(host='0.0.0.0') 
+    #app.run(port=33507)
+
+
+
 
 
 
